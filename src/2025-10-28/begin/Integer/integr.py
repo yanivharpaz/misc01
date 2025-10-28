097fbf0215a5e2caefde528b570f36a100455296
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2025-10-28 23:13:40 +0200

    initial

--- src/2025-10-28/begin/Integer/integr.py
+++
@@ -1,9 +0,0 @@
-def parse(txt):
-    """Returns a list of numbers
-    >>> parse('1,3')
-    [1, 3]
-    """
-    res = []
-    for thing in txt.split(','):
-        res.append(int(thing))
-    return res
